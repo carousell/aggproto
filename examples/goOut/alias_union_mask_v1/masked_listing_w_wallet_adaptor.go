317e296f13bc304d22baf94e9bed752cdb043cb0
vvarma <vin1990@gmail.com>
2021-05-10 18:10:46 +0800

    update examples output adaptor

--- examples/goOut/alias_union_mask_v1/masked_listing_w_wallet_adaptor.go
+++ examples/goOut/alias_union_mask_v1/masked_listing_w_wallet_adaptor.go
@@ -2,9 +2,9 @@ package alias_union_mask_v1
 
 
 import (
+	"github.com/carousell/aggproto/examples/goOut/listing"
 	"github.com/carousell/aggproto/examples/goOut/listing_comments"
 	"github.com/carousell/aggproto/examples/goOut/wallet"
-	"github.com/carousell/aggproto/examples/goOut/listing"
 )
 
 
